419e6d068cbaf069f45ab697906759d7b11bee70
Bryan Yap <bcollie115@gmail.com>
2014-01-08 00:25:02 +0000

    temporary solution to seg faults in reDistortedShape

--- src/react/Collision/Shapes/reDistortedShape.cpp
+++ src/react/Collision/Shapes/reDistortedShape.cpp
@@ -3,6 +3,22 @@
 #include "react/Entities/reEnt.h"
 #include "react/Collision/Shapes/shapes.h"
 
+namespace {
+  reShape* allocNewShape(const reShape& base) {
+    switch (base.type()) {
+      case reShape::SPHERE:
+        return new reSphere((const reSphere&)base);
+        break;
+        
+      default:
+        RE_LOG("DARN")
+        break;
+    }
+    
+    return nullptr;
+  }
+}
+
 reDistortedShape::reDistortedShape(const reShape& base)
   : _distortion(), _shape(nullptr) {
   setShape(base);
@@ -10,16 +26,19 @@ reDistortedShape::reDistortedShape(const reShape& base)
 
 reDistortedShape::~reDistortedShape() {
   if (_shape != nullptr) {
-    re::alloc_delete(_shape);
-    _shape = nullptr;
+    delete _shape;
+//    re::alloc_delete(_shape);
   }
 }
 
 void reDistortedShape::setShape(const reShape& base) {
   if (_shape != nullptr) {
-    re::alloc_delete(_shape);
+//    re::alloc_delete(_shape);
+    delete _shape;
   }
-  _shape = re::copyOf(base);
+  // TODO use the global allocator
+  _shape = allocNewShape(base);
+//  _shape = re::copyOf(base);
 }
 
 reDistortedShape& reDistortedShape::withShape(const reShape& base) {
